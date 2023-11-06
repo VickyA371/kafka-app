7c2b91ea078a0f2d77e2ce6e98c9dc2320c93b66
VickyA371 <vickyahuja371@gmail.com>
2023-11-06 21:31:00 +0530

    feat: send message to consumers from readline interface and to multiple partitions based on some conditions

--- producer/index.js
+++ producer/index.js
@@ -1,5 +1,11 @@
 const {client} = require('../client')
+const readline = require('readline')
 
+// creating cli interface to read input from user
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 const init = async () => {
     const producer = client.producer();
@@ -7,25 +13,33 @@ const init = async () => {
     console.log('connecting producer...')
     await producer.connect()
     console.log('connected with producer\n')
-    
-    console.log('sending an message...')
-    await producer.send({
-        topic: 'rider-updates',
-        messages: [
-            {
-                key: 'location-update',
-                value: JSON.stringify({
-                    name: 'Tony Stark',
-                    location: 'SOUTH'
-                })
-            }
-        ],
-    })
-    console.log('message sent...')
 
-    console.log('disconnecting producer...')
-    await producer.disconnect()
-    console.log('disconnected with producer\n')
+    rl.setPrompt('>');
+    rl.prompt();
+
+    rl.on("line", async (line) => {
+        const [name, location] = line.split(' '); // reading name and location from user input
+
+        console.log('sending an message...')
+        await producer.send({
+            topic: 'rider-updates',
+            messages: [
+                {
+                    partition: location.toLowerCase() === 'north' ? 0 : 1, // specifying partition
+                    key: 'location-update',
+                    value: JSON.stringify({
+                        name,
+                        location
+                    })
+                }
+            ],
+        })
+        console.log('message sent...')
+    }).on('close', async () => {
+        console.log('disconnecting producer...')
+        await producer.disconnect()
+        console.log('disconnected with producer\n')
+    })
 }
 
 init();
