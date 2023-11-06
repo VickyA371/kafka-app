ba0b1d76ea7699324de6659c846de925e34aa7fd
VickyA371 <vickyahuja371@gmail.com>
2023-11-06 21:33:35 +0530

    feat: create consumer group run line with cmd parameter at runtime

--- consumer/index.js
+++ consumer/index.js
@@ -1,8 +1,10 @@
 const {client} = require('../client')
 
+const group = process.argv
+
 const init = async () => {
     const consumer = client.consumer({
-        groupId: 'user-1'
+        groupId: group
     });
 
     // subscribing topic
@@ -14,7 +16,7 @@ const init = async () => {
     await consumer.run({
         eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
             // message data is in buffer type so we are using toString to convert it to string
-            console.log(`Topic: ${topic} Partition: ${partition} Message: ${message.value.toString()}`)
+            console.log(`Group: ${group} Topic: ${topic} Partition: ${partition} Message: ${message.value.toString()}`)
         }
     })
 
