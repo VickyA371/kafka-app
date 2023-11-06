bc3b50af059dfd8fd5eeaf12ff3e4d662521bc0b
VickyA371 <vickyahuja371@gmail.com>
2023-11-06 21:00:20 +0530

    chore: changes in instance creation of kafka

--- client/index.js
+++ client/index.js
@@ -1,6 +1,11 @@
 const { Kafka } = require("kafkajs");
 
 // creating instance for kafka
-const kafka = new Kafka();
+const kafka = new Kafka({
+    clientId: 'my-app', // any string that we suppose to use as our app clientId
+    brokers: [
+        '192.168.1.2:9092' // <host>/<port_addr>
+    ]
+});
 
 exports.client = kafka
